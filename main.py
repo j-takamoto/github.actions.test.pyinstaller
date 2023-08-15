3d43a0de642019018a064e09e53108d451f0c622
j-takamoto <jtakamoto@mtc-sams.net>
2023-08-15 15:38:57 +0900

    create main

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+root = tk.Tk()
+root.title("Tkinterrrrrr")
+root.geometry("640x480")
+
+lbl = ttk.Label(root, text="Hi!!!!!!!Tkinter")
+lbl.pack(anchor=tk.CENTER)
+
+root.mainloop()
